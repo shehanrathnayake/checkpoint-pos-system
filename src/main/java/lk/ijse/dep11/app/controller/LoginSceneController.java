7bb85624ad87917e3cdaeb0bc1ef81020ae778c3
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-10-14 15:41:07 +0530

    Complete loginscene UI

--- src/main/java/lk/ijse/dep11/app/controller/LoginSceneController.java
+++ src/main/java/lk/ijse/dep11/app/controller/LoginSceneController.java
@@ -1,4 +1,24 @@
 package lk.ijse.dep11.app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginSceneController {
+    public AnchorPane root;
+    public TextField txtUserId;
+    public TextField txtPassword;
+    public Button btnLogin;
+    public Button btnCancel;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
+
+
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
