0fdb234b87f608849c82c7d79b890c76b9779aef
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-10-16 02:29:29 +0530

    Complete admin dashboad UI

--- src/main/java/lk/ijse/dep11/app/controller/AdminDashboardSceneController.java
+++ src/main/java/lk/ijse/dep11/app/controller/AdminDashboardSceneController.java
@@ -1,11 +1,22 @@
 package lk.ijse.dep11.app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class AdminDashboardSceneController {
     public Label lblLoggedUserName;
+    public ImageView imgProfileImage;
+    public Label lblUserId;
+    public Label lblUserName;
+    public Button btnLogout;
+    public ImageView imgViewLogo;
 
     public void initialize() {
         lblLoggedUserName.setText(UserDetails.getLoggedUser().getFirstName());
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
     }
 }
